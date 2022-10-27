cd3fc6e392f34b2b2492156dfec6375e893dc750
Arnold Siyabonga Mdluli <arnoldmdluli00@gmail.com>
2022-10-27 00:30:52 +0000

    I skipped task 3 so I fixed it

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,10 @@ from datetime import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
-        if kwargs:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            del kwargs["__class__"]
-            self.__dict__.update(kwargs)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+           self.id = str(uuid.uuid4())
+           self.created_at = datetime.now()
+           self.updated_at = datetime.now()
+
     def __str__(self):
         """Returns a string representation of the instance"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
