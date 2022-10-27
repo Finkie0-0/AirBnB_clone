735629f84c52b2f1a1617c7733eb9d991605a703
loveth10 <lovethokebe@gmail.com>
2022-10-27 08:14:05 +0000

    task3 has been fixed

--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,23 @@
 
 import uuid
 from datetime import datetime
+import models
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
+       if kwargs:
+           kwargs["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+           kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+           del kwargs["__class__"]
+           self.__dict__ .update(kwargs)
+       else:
            self.id = str(uuid.uuid4())
            self.created_at = datetime.now()
            self.updated_at = datetime.now()
+           models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -18,6 +28,7 @@ class BaseModel:
     def save(self):
         """Updates the attribute (updated_at) with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save[]
     
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
