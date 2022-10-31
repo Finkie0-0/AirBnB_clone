9b83ee60b2666c1482c473d22f2570fac2e7ebdd
loveth10 <lovethokebe@gmail.com>
2022-10-31 00:16:51 +0000

    adding more fix and modules

--- models/state.py
+++ models/state.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-""" The State module """
+"""
+module state contains class state
+and inherits from class BaseModel
+"""
+
+
 from models.base_model import BaseModel
 
-class State(BaseModel):
-     """ The class State """
-     name = ""
 
+class State(BaseModel):
+    """
+    State class for state instances
+    """
+
+    name = ""
